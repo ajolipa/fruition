b6c1d3665d4d8994398cc2795ab1b174f20408c6
ajolipa <rebecca.ann.black@gmail.com>
2023-01-11 14:52:15 -0500

    troubleshooting php

--- processmessage.php
+++ processmessage.php
@@ -1,13 +1,16 @@
 <?php
 if ($_POST["submit"]) {
-    $sent = mail("ajolipa@gmail.com", "Testing", $_POST["message"], "From: " . $_POST["email"])
-    if ($sent) {
-        header("Location: https://www.fruitiondining.com/hub.html?contact=true#contact")
-    }
-    else {
-        header("Location: https://www.fruitiondining.com/hub.html?contact=false#contact")
-    }
+    // $sent = mail("ajolipa@gmail.com", "Testing", $_POST["message"], "From: " . $_POST["email"])
+    // if ($sent) {
+    //     header("Location: https://www.fruitiondining.com/hub.html?contact=true#contact")
+    // }
+    // else {
+    //     header("Location: https://www.fruitiondining.com/hub.html?contact=false#contact")
+    // }
+    echo $_POST["message"]
 }
-
+else {
+    header("Location: https://www.fruitiondining.com/hub.html")
+}
 
 ?>
