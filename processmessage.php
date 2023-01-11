21e49780ae80f2cc22494facf2833bc1d4485b10
ajolipa <rebecca.ann.black@gmail.com>
2023-01-10 19:19:50 -0500

    added php form processing

---
+++ processmessage.php
@@ -0,0 +1,13 @@
+<?php
+if ($_POST["submit"]) {
+    $sent = mail("ajolipa@gmail.com", "Testing", $_POST["message"], "From: " . $_POST["email"])
+    if ($sent) {
+        header("Location: https://www.fruitiondining.com/hub.html?contact=true#contact")
+    }
+    else {
+        header("Location: https://www.fruitiondining.com/hub.html?contact=false#contact")
+    }
+}
+
+
+?>
